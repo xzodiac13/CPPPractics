530c205c0f021fd284b2d23c08cf9e417ceaf1de
Alexander <75498985+xzodiac13@users.noreply.github.com>
2023-03-12 16:42:08 +0300

    Added 1 practic

---
+++ Факториал рекурсией.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int Factorial(int N)
+{
+	if ((N == 0) || (N == 1))
+		return 1;
+
+	return N * Factorial(N - 1);
+}
+
+void main()
+{
+	int j;
+	std::cin >> j; 
+	std::cout << Factorial(j);
+}
