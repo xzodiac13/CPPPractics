ca6e0955bb303c57ce730161da6a34f44f50c454
Alexander <75498985+xzodiac13@users.noreply.github.com>
2023-03-13 22:43:35 +0300

    Add files via upload

---
+++ Вывод эксепшенов.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <exception>
+#include <stdexcept>
+
+double divide(int, int);
+
+int main()
+{
+    int x = 500;
+    int y = 0;
+    try
+    {
+        double z = divide(x, y);
+        std::cout << z << std::endl;
+    }
+    catch (std::overflow_error err)
+    {
+        std::cout << "Overflow_error: " << err.what() << std::endl;
+    }
+    catch (std::runtime_error err)
+    {
+        std::cout << "Runtime_error: " << err.what() << std::endl;
+    }
+    catch (std::exception err)
+    {
+        std::cout << "Exception!!!" << std::endl;
+    }
+    std::cout << "The End..." << std::endl;
+    return 0;
+}
+
+double divide(int a, int b)
+{
+    if (b == 0)
+        throw std::runtime_error("Division by zero!");
+    return a / b;
+}
