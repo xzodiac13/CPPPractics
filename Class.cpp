597c896d53327c00a96113dd952b5f69917618a1
Alexander <75498985+xzodiac13@users.noreply.github.com>
2023-03-13 22:42:45 +0300

    Add files via upload

---
+++ Class.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+class Person
+{
+public:
+    class Account  
+    {
+    public:
+        Account(const std::string& p_email, const std::string& p_password)
+        {
+            email = p_email;
+            password = p_password;
+        }
+        std::string email{};
+        std::string password{};
+    };
+    Person(const std::string& p_name, const Account& p_account)
+    {
+        name = p_name;
+        account = p_account;
+    }
+    void print()
+    {
+        std::cout << "Name: " << name << "\n" << "Email: " << account.email << "\n"
+            << "Password: " << account.password << std::endl;
+
+    }
+private:
+    std::string name;
+    Account account{ "", "" }; 
+};
+
+int main()
+{
+    Person::Account account{ "bob@somemail.com", "qwerty" };
+    Person bob{ "Bob", account };
+    bob.print();
+}
