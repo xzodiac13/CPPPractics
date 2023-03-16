ec45bfdbf58f8141142a39df769a0f561366a053
Alexander <75498985+xzodiac13@users.noreply.github.com>
2023-03-16 14:43:58 +0300

    Swap

---
+++ Свап переменных указателями.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void replace(int* pa, int* pb)
+{
+	int* pc;
+	pc = new int;
+	(*pc) = *pa;
+	(*pa) = *pb;
+	(*pb) = *pc;
+
+}
+
+void main()
+{
+	int a;
+	int b;
+	cin >> a >> b;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+
+	cout << "Swap processing..." << endl;
+	replace(&a, &b);
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "Swap completed!"<< endl;
+	cout << "a - b = " << a << " - " << b << " = " << a - b << endl;
+}
