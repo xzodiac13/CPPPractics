b42ca9c3deecab0ef001ab8927dec5abde8d0f06
Alexander <75498985+xzodiac13@users.noreply.github.com>
2023-03-30 15:01:29 +0300

    EC

---
+++ Error catcher.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void divide(float a, float b)
+{
+	if (b == 0) throw 100;
+	cout << a / b << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	try {
+		divide(5.2, 0.0);
+	}
+	catch (int err) {
+		if (err == 100) cout <<
+			"Ошибка при делении на ноль!";
+	}
+
+
+
+	return 0;
+}
