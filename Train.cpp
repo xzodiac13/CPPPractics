f048efc4ec8f0a2c1956a8cca683af1d36da6284
Alexander <75498985+xzodiac13@users.noreply.github.com>
2023-03-30 13:52:53 +0300

    Via

---
+++ Train.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void minimal(int* arr, int size);
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	
+	int arr[] = { 5, 7, 3, 5, -2 };
+	minimal(arr, 5);
+}
+
+void minimal(int* arr, int size)
+{
+	int min = *arr;
+	for (int i = 0; i < size; i++)
+	{
+		if (min > *(arr + i))
+			min = *(arr + i);
+
+	}
+	cout << "Minimal Element: " << min << endl;
+}
